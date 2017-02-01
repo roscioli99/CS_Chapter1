ee8db22704d0aaa7048b8ff838baa18b363acbae
roscioli99 <ryanroscioli99@gmail.com>
2017-02-01 13:48:13 -0500

    Finished Chapter 1

---
+++ CS_Chapter1/BurmaShave/BurmaShave.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurmaShave
+{
+    class BurmaShave
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tobacco stains");
+            Console.WriteLine("And stale breath too");
+            Console.WriteLine("Are two of the things");
+            Console.WriteLine("It takes from you");
+            Console.WriteLine("Burma-Vita Tooth Powder");
+        }
+    }
+}
